701bb132c59c43b9251720216d66886cc5c60793
magnetopython <2640520498@qq.com>
2023-08-30 16:12:39 +0800

    cm1

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -34,6 +36,12 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+// 处理server回应的消息，直接显示到标准输出
+func (client *Client) DealResponse() {
+	//一旦client.conn有数据，就直接copy到stdout,永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+}
+
 func (client *Client) menu() bool {
 	var flag int
 
@@ -59,17 +67,58 @@ func (client *Client) Run() {
 
 		switch client.flag {
 		case 1:
-			fmt.Println("公聊模式")
+			// fmt.Println("公聊模式")
+			client.PublicChat()
 			break
 		case 2:
 			fmt.Println("私聊模式")
 			break
 		case 3:
-			fmt.Println("更新用户名")
+			// fmt.Println("更新用户名")
+			client.UpdateName()
 			break
 
 		}
 	}
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println("please input username")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+	return true
+}
+
+func (client *Client) PublicChat() {
+
+	var chatMsg string
+
+	//提示用户输入消息
+	fmt.Println("please input chat message,input exit to exit")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn Write err:", err)
+				break
+			}
+		}
+
+		chatMsg = ""
+		fmt.Println("please input chat message,input exit to exit")
+		fmt.Scanln(&chatMsg)
+	}
+
+	//发给服务器
 }
 
 var serverIp string
@@ -90,6 +139,9 @@ func main() {
 		fmt.Println("connect server failed")
 		return
 	}
+
+	//单独开启一个goroutine
+	go client.DealResponse()
 
 	fmt.Println("connect server successfully")
 
